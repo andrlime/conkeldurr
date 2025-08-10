358c4c4e9673c51ec3f3a76ed77b6a8e7b3bd2b0
Andrew Li <andrewli@u.northwestern.edu>
2025-08-10 15:22:24 -0500

    refactor the tests too

--- test/expect_tests/spreadsheet_parser_tests.ml
+++ test/expect_tests/spreadsheet_parser_tests.ml
@@ -4,7 +4,7 @@ let%expect_test "parse string headers" =
   let header = "(String abc)" in
   header
   |> Spreadsheet.Parser.parse_column_header
-  |> Spreadsheet.Header.header_to_string
+  |> Spreadsheet.Header.to_string
   |> print_endline;
   [%expect {| String abc |}]
 ;;
@@ -13,7 +13,7 @@ let%expect_test "parse float headers" =
   let header = "(Float abc)" in
   header
   |> Spreadsheet.Parser.parse_column_header
-  |> Spreadsheet.Header.header_to_string
+  |> Spreadsheet.Header.to_string
   |> print_endline;
   [%expect {| Float abc |}]
 ;;
