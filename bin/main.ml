245937f4903d93ef35186d943f37bdaaead59244
Andrew Li <andrewli@u.northwestern.edu>
2025-08-10 01:25:43 -0500

    collapse some files in the main module

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,23 @@
+open Conkeldurr
 open Cmdliner
 
-let main () = Cmd.eval Commands.main_cmd
+let conkeldurr_process_file file =
+  file
+  |> Io.set_working_directory
+  |> Io.read_file
+  |> Program.T.of_string
+  |> Interpreter.T.interpret
+;;
+
+let input_file_arg =
+  let doc = "A file to interpret, provided via --input or -i." in
+  Arg.(required & opt (some file) None & info [ "i"; "input" ] ~docv:"PATH" ~doc)
+;;
+
+let main_cmd =
+  let term = Term.(const conkeldurr_process_file $ input_file_arg) in
+  Cmd.v (Cmd.info "conkeldurr" ~version:"%%VERSION%%") term
+;;
+
+let main () = Cmd.eval main_cmd
 let () = if !Sys.interactive then () else exit (main ())
